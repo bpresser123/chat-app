eb93250f777f7ebf6ed8eb73296c87642756cec8
Brian Presser <bmpresser@gmail.com>
2020-08-05 16:38:59 -0700

    Adding users and socket connections

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,10 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import querystring from 'query-string';
+import queryString from 'query-string';
 import io from 'socket.io-client';
 
 import './chat.css';
 
-const Chat = () => {
+let socket;
+
+const Chat = ({ location }) => {
+
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const ENDPOINT = 'localhost:5000'
+
+  useEffect(() => {
+    const { room, name } = queryString.parse(location.search);
+
+    setName(name);
+    setRoom(room);
+
+    socket = io(ENDPOINT);
+    socket.emit('join', { name, room }, () => {
+    });
+
+    return () => {
+      socket.emit('disconnect');
+      socket.off();
+    }
+  }, [ENDPOINT, location.search]);
+
   return (
     <h2>Chat</h2>
   )
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
 const PORT = process.env.PORT || 5000;
 
@@ -12,6 +13,21 @@ const io = socketio(server);
 
 io.on('connection', (socket) => {
   console.log('New connection')
+
+  socket.on('join', ({ name, room }, callback) => {
+    const { user, error } = addUser({ id: socket.id, name, room });
+
+    if(error) {
+      return callback(error);
+    }
+
+    socket.emit('message', { user: 'admin', text: `${user.name} welcome to the ${user.room} room`})
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined the ${user.room} room`})
+
+    socket.join(user.room)
+
+    callback();
+  })
 
   socket.on('disconnect', () => {
     console.log('User left')
