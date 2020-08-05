a8f2b7e2bd0e69f343f98c564e4bb379f21873f9
Brian Presser <bmpresser@gmail.com>
2020-08-05 15:47:38 -0700

    Adding input boxes, buttons, and styles to Join component

--- client/src/components/Chat.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Chat = () => {
-  return (
-    <h2>Chat</h2>
-  )
-}
-
-export default Chat;
---
+++ client/src/components/Chat/Chat.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import querystring from 'query-string';
+import io from 'socket.io-client';
+
+import './chat.css';
+
+const Chat = () => {
+  return (
+    <h2>Chat</h2>
+  )
+}
+
+export default Chat;
